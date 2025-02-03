f08944d1c9f9cca980003b659209e5166743a90d
Jefferson <44975876+ZX-80@users.noreply.github.com>
2025-02-03 01:13:53 +0000

    refactor

--- bookmark_all.py
+++ bookmark_all.py
@@ -4,22 +4,30 @@
 This script creates bookmarks for all snapshots matching some pattern.
 """
 
+import argparse
 import libzfs
 import re
 
 from contextlib import suppress
 
-SNAPSHOT_PATTERN = re.compile(r"your_pattern_here")
 
-def bookmark_all() -> int:
-    """Create bookmarks for all snapshots matching some pattern."""
-    bookmarks_created = 0
-    for snapshot in libzfs.ZFS().snapshots:
-        if SNAPSHOT_PATTERN.search(f"{snapshot.parent.name}@{snapshot.snapshot_name}"):
-            with suppress(FileExistsError):  # Skip if bookmark already exists
-                snapshot.bookmark(snapshot.snapshot_name)
-                bookmarks_created += 1
-    return bookmarks_created
+def bookmark_all(snapshot_pattern: re.Pattern) -> int:
+    """Create bookmarks for all snapshots matching some pattern."""
+    bookmarks_created = 0
+    for snapshot in libzfs.ZFS().snapshots:
+        if snapshot_pattern.search(f"{snapshot.parent.name}@{snapshot.snapshot_name}"):
+            with suppress(FileExistsError):  # Skip if bookmark already exists
+                snapshot.bookmark(snapshot.snapshot_name)
+                bookmarks_created += 1
+    return bookmarks_created
+
 
 if __name__ == "__main__":
-    print(f"Created {bookmark_all()} bookmark(s)")
+    parser = argparse.ArgumentParser(description=bookmark_all.__doc__)
+    parser.add_argument(
+        "snapshot_pattern",
+        type=re.compile,
+        help='The regex pattern to compare with snapshots of the form "parent@name"',
+    )
+    print(f"Created {bookmark_all(parser.parse_args().snapshot_pattern)} bookmark(s)")
+
